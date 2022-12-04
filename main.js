f88eb2b83ac166d750476e4af8117b7f23072f74
jpblicer <jpblicer@gmail.com>
2022-12-04 20:30:50 +0900

    added xyz Item variables, created newStory variable that replaces storyText with xyzItem variables.

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+
+
+
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
@@ -28,3 +31,33 @@ const insertz = [
 'melted into a puddle on the sidewalk',
 'turned into a slug and crawled away'
 ];
+
+const xItem = randomValueFromArray(insertx);
+const yItem = randomValueFromArray(inserty);
+const zItem =  randomValueFromArray(insertz);
+
+const newStory = storyText
+.replace(':insertx:' , xItem)
+.replace(':inserty:' , yItem)
+.replace(':insertz:' , zItem);
+
+/*
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
+*/
